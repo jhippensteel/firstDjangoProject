c53359e5cc8d245b7d9e8d48a05713878101d6a3
Jaysen Hippensteel <105116606+jhippensteel@users.noreply.github.com>
2024-02-17 13:44:55 -0500

    Initial Commit

---
+++ firstDjangoProject/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Member
+
+# Register your models here.
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "join_date",)
+
+
+admin.site.register(Member, MemberAdmin)
